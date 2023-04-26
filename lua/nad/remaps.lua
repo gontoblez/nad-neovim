6c3e9db559e69ee26263ad4f324f02d52384ff5c
gontoblez <gontoblez@gmail.com>
2023-04-26 09:14:17 +0300

    configured: remaps.lua comments

--- lua/nad/remaps.lua
+++ lua/nad/remaps.lua
@@ -1,12 +1,12 @@
-local keymap = vim.keymap
+--local keymap = vim.keymap
 vim.g.mapleader = " "           -- set mod key to <SPACE>
 
 
 -- vim.keymap.set("<mode>", "<keys", <command>)
 --
 -- NORMAL MODE REMAPS
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)          -- netrw with <ldr>pv
-vim.keymap.set("n", "<leader>e", ":Lex 30<cr>")        -- netrw in left pane (change soon)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)           -- netrw with <ldr>pv
+vim.keymap.set("n", "<leader>e", ":Lex 30<cr>")         -- netrw in left pane (change soon)
 
 -- Navigate windows
 vim.keymap.set("n", "<C-h>", "<C-w>h")
@@ -15,7 +15,7 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- INSERT MODE REMAPS
-vim.keymap.set("i", "jk", "<ESC>") -- Enter NORMAL mode with "jk"
+vim.keymap.set("i", "jk", "<ESC>")                      -- Enter NORMAL mode with "jk"
 
 -- VISUAL MODE REMAPS
 -- stay in indent mode
@@ -26,4 +26,4 @@ vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<A-j>", ":m .+1<CR>==")
 
 -- better buffer management
-vim.keymap.set("v", "p", '"_dP')                       -- paste over & don't yank again
+vim.keymap.set("v", "p", '"_dP')                        -- paste over & don't yank again
