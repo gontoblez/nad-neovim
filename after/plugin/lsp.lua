6d4f13c3b769ca1cdb50cd9d98c7f58972e7ad79
gontoblez <gontoblez@gmail.com>
2023-04-26 11:44:35 +0300

    fixed: misspelled function name resulted in error.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -45,10 +45,9 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gl", function () vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" }) end, opts)
+  vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
