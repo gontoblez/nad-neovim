404c7461dc330efbf2c3f85a458b14c0c1385502
gontoblez <gontoblez@gmail.com>
2023-04-26 11:41:26 +0300

    configured: treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -15,12 +15,16 @@ require'nvim-treesitter.configs'.setup {
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
+  -- list of parsers to ignore installing
+  ignore_install = { "" },
+
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
   highlight = {
     enable = true,
-
+    disable = { "" }, -- a list of languages that will not be highlighted with treesitter
+    additional_vim_regex_highlighting = true,
   },
 }
