72ad5c60b0aa1e19801567e98ed297254387e91d
gontoblez <gontoblez@gmail.com>
2023-04-27 16:31:08 +0300

    removed: ts-rainbow2 because treesitter is crashing when using it. yea L, ik

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,12 @@
-require'nvim-treesitter.configs'.setup {
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    print("Treesitter is not ok")
+    return
+end
+
+
+
+treesitter.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = {
         "latex",
@@ -32,15 +40,6 @@ require'nvim-treesitter.configs'.setup {
         disable = {
             "yaml"
         }
-    },
-    rainbow = {
-        enable = true,
-        -- list of languages you want to disable the plugin for
-        disable = { 'jsx', 'cpp' },
-        -- Which query to use for finding delimiters
-        query = 'rainbow-parens',
-        -- Highlight the entire buffer all at once
-        strategy = require('ts-rainbow').strategy.global,
     },
 --[[    context_commentstring {
         enable = true,
