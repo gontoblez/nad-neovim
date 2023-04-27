9ca66740ba1e89d73bfeb374f5ec670e0a0cddac
gontoblez <gontoblez@gmail.com>
2023-04-27 18:39:08 +0300

    formatted: packer.lua

--- lua/nad/packer.lua
+++ lua/nad/packer.lua
@@ -1,114 +1,117 @@
 -- Automatically install packer
-local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = vim.fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.nvim",
-        install_path,
-    }
-    print("Installing packer.. Reopen Neovim!")
-    vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = vim.fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer.. Reopen Neovim!")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Automatically reload neovim when saving packer.lua file
-vim.cmd [[
+vim.cmd([[
 augroup packer_user_config
 autocmd!
 autocmd BufWritePost packer.lua source <afile> | PackerSync
 augroup end
-]]
+]])
 
 -- Use a protected call so nvim doesn't scream on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-    print("Well, neovim doesn't like this")
-    return
+	print("Well, neovim doesn't like this")
+	return
 end
 
 -- use a popup window for packer
-packer.init {
-    display = {
-        open_fn = function()
-            return require("packer.util").float { border = "rounded" }
-        end,
-    },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- Plugins here
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'                    -- Packer can manage itself
-    use "nvim-lua/popup.nvim"                       -- popup API
-    use "nvim-lua/plenary.nvim"                     -- plenary
-    -- telescope
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use "vim-scripts/gruvbox"                       -- gruvbox colorscheme
-    use("luisiacc/gruvbox-baby")                    -- supposedly better gruvbox
-    use("ThePrimeagen/harpoon")                     -- harpoon
-    use 'vim-airline/vim-airline'
-    use 'vim-airline/vim-airline-themes'
-    --treesitter
-    use('nvim-treesitter/nvim-treesitter',
-        {run = ':TSUpdate'})
-    -- auotpairs
-    use("windwp/nvim-autopairs")
-    -- commenting
-    use("numToStr/Comment.nvim")
-    use("JoosepAlviste/nvim-ts-context-commentstring")
-    -- nvim tree
-    use("nvim-tree/nvim-tree.lua")
-    use("nvim-tree/nvim-web-devicons")
-    -- Gitsigns
-    use("lewis6991/gitsigns.nvim")
-    -- LaTeX Support
-    use 'lervag/vimtex'
-    -- markdown
-    use 'gabrielelana/vim-markdown'
-    -- markdown preview
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    })
-    use 'mbbill/undotree'
-    use 'tpope/vim-fugitive'
-    -- LSP
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},              -- Required
-            {                                       -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            {'williamboman/mason-lspconfig.nvim'},  -- Optional
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim") -- Packer can manage itself
+	use("nvim-lua/popup.nvim") -- popup API
+	use("nvim-lua/plenary.nvim") -- plenary
+	-- telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("vim-scripts/gruvbox") -- gruvbox colorscheme
+	use("luisiacc/gruvbox-baby") -- supposedly better gruvbox
+	use("ThePrimeagen/harpoon") -- harpoon
+	use("vim-airline/vim-airline")
+	use("vim-airline/vim-airline-themes")
+	--treesitter
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	-- auotpairs
+	use("windwp/nvim-autopairs")
+	-- commenting
+	use("numToStr/Comment.nvim")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+	-- nvim tree
+	use("nvim-tree/nvim-tree.lua")
+	use("nvim-tree/nvim-web-devicons")
+	-- Gitsigns
+	use("lewis6991/gitsigns.nvim")
+	-- LaTeX Support
+	use("lervag/vimtex")
+	-- markdown
+	use("gabrielelana/vim-markdown")
+	-- markdown preview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	-- LSP
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{
+				-- Optional
+				"williamboman/mason.nvim",
+				run = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},                   -- Required
-            {'hrsh7th/cmp-buffer'},                 -- Optional
-            {'hrsh7th/cmp-path'},                   -- Optional
-            {'hrsh7th/cmp-cmdline'},                -- Optional
-            {'hrsh7th/cmp-nvim-lsp'},               -- Required
-            {'hrsh7th/cmp-nvim-lua'},               -- Optional
-            {'saadparwaiz1/cmp_luasnip'},           -- Optional
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-buffer" }, -- Optional
+			{ "hrsh7th/cmp-path" }, -- Optional
+			{ "hrsh7th/cmp-cmdline" }, -- Optional
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
+			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},                   -- Required
-            {'rafamadriz/friendly-snippets'},       -- Optional
-        }
-    }
-    use("jose-elias-alvarez/null-ls.nvim")          -- for formatters and linters
-    --- Automatically load config after cloning packer.nvim
-    if PACKER_BOOTSTRAP then
-        require("packer").sync()
-    end
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" }, -- Optional
+		},
+	})
+	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
+	--- Automatically load config after cloning packer.nvim
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
