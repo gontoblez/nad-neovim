731691d08d04b12ac41e1ad46fd6ec6c5887c878
gontoblez <gontoblez@gmail.com>
2023-04-28 08:28:59 +0300

    added: useful comments to null-ls.lua

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -13,14 +13,14 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		formatting.beautysh,
-		formatting.latexindent,
-		diagnostics.chktex,
-		diagnostics.shellcheck,
-		diagnostics.flake8,
+		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }), -- install with npm
+		formatting.black.with({ extra_args = { "--fast" } }), -- install with pip
+		formatting.stylua,          -- install with cargo
+		formatting.beautysh,        -- install with pip
+		formatting.latexindent,     -- if you have texlive-full installed, you have this package
+		diagnostics.chktex,         -- same with latexindent
+		diagnostics.shellcheck,     -- install with apt (on debian)
+		diagnostics.flake8,         -- install with pip
 	},
 })
 
